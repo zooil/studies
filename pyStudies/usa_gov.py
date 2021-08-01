e58758c4e54aebf8e5c8379c18ca3b556dd2f76a
Zoolian <m@zooil.com>
2021-08-01 23:29:52 +0900

    aa

---
+++ pyStudies/usa_gov.py
@@ -0,0 +1,13 @@
+import json
+import pandas as pd
+from pandas import DataFrame, Series
+
+path = 'datasets/bitly_usagov/example.txt'
+records = [json.loads(line) for line in open(path)]
+
+frame = DataFrame(records)
+tz_counts = frame['tz'].value_counts()
+cl_tz = frame['tz'].fillna('Missing')
+cl_tz[cl_tz == ''] = 'Unknown'
+tz_counts = cl_tz.value_counts()
+tz_counts[:10].plot(kind='barh', rot=0)
