26d1db6f01a4db7b61cba065c039129867ab220b
Zoolian <m@zooil.com>
2022-09-07 11:41:06 +0900

    update

--- ipython/q5_jword_study.py
+++ ipython/q5_jword_study.py
@@ -3,20 +3,29 @@ from pynput import keyboard
 
 
 def on_release(key):
-    print('Key %s released' %key)
     if key == keyboard.Key.space: 
         checkWord()
     if key == keyboard.Key.esc: #esc 키가 입력되면 종료
         return False
 
 def checkWord():
-	f	= open("japan_word.txt", 'r')
+	f	= open("jword.txt", 'r')
 	lines	= f.readlines()
 	lineLen	= len(lines)
 	randNum	= random.randint(0,lineLen)
-	wordList= lines[randNum].strip().split('\t')
-	wordList[0].strip()
-	print(wordList)
+	str = lines[randNum]
+	cnt = 1
+	if lineLen == randNum:
+		cnt = -1
+	while True:
+		if '예)' in str:
+			break
+		else:
+			randNum += cnt
+			str = lines[randNum]
+	print(lines[randNum-1])
+	print(lines[randNum])
+	print(lines[randNum+1])
 	f.close()
 
  # 리스너 등록방법1
