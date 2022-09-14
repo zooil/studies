620d2c86e506aab9019d58cd941a7e97c8c6c0bf
Zoolian <m@zooil.com>
2022-09-14 15:29:17 +0900

    update

--- ipython/q4_eng_word.py
+++ ipython/q4_eng_word.py
@@ -3,20 +3,20 @@ from pynput import keyboard
 
 
 def on_release(key):
-    print('Key %s released' %key)
+#    print('Key %s released' %key)
     if key == keyboard.Key.space: 
         checkWord()
     if key == keyboard.Key.esc: #esc 키가 입력되면 종료
         return False
 
 def checkWord():
-	f	= open("japan_word.txt", 'r')
+	f	= open("cherry01.txt", 'r')
 	lines	= f.readlines()
 	lineLen	= len(lines)
-	randNum	= random.randint(0,lineLen)
-	wordList= lines[randNum].strip().split('\t')
-	wordList[0].strip()
-	print(wordList)
+	randNum	= random.randint(0,lineLen-1)
+	wordList= lines[randNum].strip().split(',')
+#	wordList[0].strip()
+	print(wordList,'\n')
 	f.close()
 
  # 리스너 등록방법1
